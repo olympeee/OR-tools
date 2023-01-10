c08bbdca0a28ff3edcfbdef79af74bfe5755ee2c
Hugo Prenot <62989349+HugoPrenot@users.noreply.github.com>
2023-01-10 12:25:47 +0100

    CREATION PROJET VIDE

---
+++ Sudoku.ColorSolver/ColorSolver.cs
@@ -0,0 +1,13 @@
+﻿using Sudoku.Shared;
+
+namespace Sudoku.ColorSolver
+{
+    public class ColorSolver : ISudokuSolver
+    {
+    
+        SudokuGrid ISudokuSolver.Solve(SudokuGrid s)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
