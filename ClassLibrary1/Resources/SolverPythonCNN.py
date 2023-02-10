a87e52092637f8a42751b295daef163d5bc52ef6
AYMERIC ECOLE <aymeric.moulet@edu.ece.fr>
2023-02-10 17:37:28 +0100

    implementation du code python

---
+++ ClassLibrary1/Resources/SolverPythonCNN.py
@@ -0,0 +1,18 @@
+import copy
+import keras
+import numpy as np
+from model import get_model
+from data_preprocess import *
+
+
+   
+Sudoku = solve_sudoku(Sudoku.Cells)
+
+    
+
+
+
+
+
+
+
---
+++ Sudoku.Solution/GenericSolver.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sudoku.Shared;
+
+namespace Sudoku.Solution
+{
+    public class GenericSolver : ISudokuSolver
+    {
+        public SudokuGrid Solve(SudokuGrid s)
+        {
+            return s.CloneSudoku();
+        }
+
+    }
+}
